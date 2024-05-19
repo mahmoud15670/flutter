0319529fd4f466977a5237971bf614ba2f648d4e
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-05-19 18:46:13 +0000

    May 19, 2024, 9:46 PM

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -23,7 +23,6 @@ class HomePage extends StatelessWidget {
         title: const Text('Register'),
         ),
       body: TextButton (onPressed: () => {
-
       },child: const Text('register'),),
     );
   }
