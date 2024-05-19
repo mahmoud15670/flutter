645b3bff7e277f804f5564da80fc91715ae30de1
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-05-19 22:20:13 +0300

    a

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -24,6 +24,20 @@ class _HomePageState extends State<HomePage> {
 	late final TextEditingController _email;
 	late final TextEditingController _password;
 
+	@override
+  void initState() {
+	_email = TextEditingController();
+	_password = TextEditingController();
+    super.initState();
+  }
+
+
+	@override
+  void dispose() {
+	_email.dispose();
+	_password.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
