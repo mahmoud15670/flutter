b992d6be4e7fa7dfb10a2b627cdfec284e422742
mahmoud15670 <mahmoud.ghalab15@gmail.com>
2024-05-20 10:03:17 +0000

    May 20, 2024, 1:03 PM

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -62,8 +62,9 @@ class _HomePageState extends State<HomePage> {
 				onPressed: () async {
           final email = _email.text;
           final password = _password.text;
-          FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
-				},
+          final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+          print(userCredential);
+        },
 				child: const Text('register'),
 				),
         ],
